5d39d4a3bfd5188490a91d304e56fa4bbb2a7202
ildus523 <ildus523@mail.ru>
2011-05-14 17:25:52 +0400

    Finised generate files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110512211909) do
+ActiveRecord::Schema.define(:version => 20110514132040) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,36 @@ ActiveRecord::Schema.define(:version => 20110512211909) do
     t.datetime "updated_at"
     t.string   "excerpt"
     t.string   "location"
+    t.integer  "user_id"
+  end
+
+  create_table "articles_categories", :id => false, :force => true do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "profiles", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.date     "birthday"
+    t.text     "bio"
+    t.string   "color"
+    t.string   "twitter"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
