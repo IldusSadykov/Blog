65846c76c4da4239b2e19cb3cc1bd870431598ac
ildus523 <ildus523@mail.ru>
2011-05-17 09:37:16 +0400

    Finish sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Blog::Application.routes.draw do
     resources :comments
   end
   resources :users
+  resource :session
+  match '/login' => "sessions#new", :as => "login"
+  match '/logout' => "sessions#destroy", :as => "logout"
+
 
 
   # The priority is based upon order of creation:
